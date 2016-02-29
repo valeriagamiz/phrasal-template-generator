38c9d0b2811394004d7b9f50a63075d17183aa38
valeriagamiz <Valeria@Valerias-MacBook-Pro.local>
2016-02-29 15:47:12 -0500

    IF-STATEMENTS

--- js/main.js
+++ js/main.js
@@ -19,3 +19,15 @@ anAdjective = prompt('An adjective (beautiful, super, etc.)');
 aPast = prompt('A past tense verb (ran, burped, etc.)');
 
 aStory = prompt('Which story would you like?\n a) Snacking\n b) Escaping\n c) Winning');
+
+if (aStory == 'a') {
+  document.write('After snacking on '  + anAdjective + ' treats, the ' + aColour + ' bellied ' + aCreature + ' ' + aPast + ' for hours.');
+}
+
+if (aStory == 'b') {
+  document.write('Ameilia ' + aPast + ' through the ' + anAdjective + ' ' + aColour + ' nebula escaping the space ' + aCreature + '.');
+}
+
+if (aStory == 'c') {
+  document.write('Jackson chose his ' + anAdjective + ',' + aColour + ' ' + aCreature + ' card and ' + aPast + ' it to the table knowing he won.');
+}
