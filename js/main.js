dbe9984dc543cd376a712069f41d544c82b3f694
valeriagamiz <Valeria@Valerias-MacBook-Pro.local>
2016-02-29 15:46:45 -0500

    Create .js
    wrote alert and   variations

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var aColour;
+
+var aCreature;
+
+var anAdjective;
+
+var aPast;
+
+var aStory;
+
+alert('Let’s make a story together!');
+
+aColour = prompt('A colour (orange, indigo, etc.)');
+
+aCreature = prompt('A creature (dragon, moth, etc.)');
+
+anAdjective = prompt('An adjective (beautiful, super, etc.)');
+
+aPast = prompt('A past tense verb (ran, burped, etc.)');
+
+aStory = prompt('Which story would you like?\n a) Snacking\n b) Escaping\n c) Winning');
